80ef6ed4ca4ac9e297a92b10fb78745505c00fbc
attilakaradeniz <attilakaradeniz@gmail.com>
2021-10-28 16:01:41 +0200

    index, main, styles modified

---
+++ main.js
@@ -0,0 +1,43 @@
+let mousePosition;
+let offset = [0,0];
+let span;
+let isDown = false;
+
+
+ span = document.createElement("span");
+ span.className += "testus";
+span.style.position = "absolute";
+span.style.left = "70px";
+span.style.top = "0px";
+span.style.width = "100px";
+span.style.height = "100px";
+span.style.background = "ywellow";
+span.style.color = "blue";
+
+document.body.appendChild(span);
+
+span.addEventListener('mousedown', function(e) {
+    isDown = true;
+    offset = [
+        span.offsetLeft - e.clientX,
+        span.offsetTop - e.clientY
+    ];
+}, true);
+
+document.addEventListener('mouseup', function() {
+    isDown = false;
+}, true);
+
+document.addEventListener('mousemove', function(event) {
+    event.preventDefault();
+    if (isDown) {
+        mousePosition = {
+
+            x : event.clientX,
+            y : event.clientY
+
+        };
+        span.style.left = (mousePosition.x + offset[0]) + 'px';
+        span.style.top  = (mousePosition.y + offset[1]) + 'px';
+    }
+}, true);
