68527216d150b6638ef4a5d63865c6f6d3457004
attilakaradeniz <attilakaradeniz@gmail.com>
2021-11-04 09:14:15 +0100

    jpgs added changer added, still tinkering, testing

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ let mousePosition;
 let offset = [0,0];
 let span;
 let isDown = false;
-
-
+let photoArray = [0, 1, 2, 3];
+let photoCounter = 0;
+let photoString = "";
  span = document.createElement("span");
  span.className += "testus";
 span.style.position = "absolute";
@@ -41,3 +42,16 @@ document.addEventListener('mousemove', function(event) {
         span.style.top  = (mousePosition.y + offset[1]) + 'px';
     }
 }, true);
+
+let myFunc;
+function iterate() {
+    photoCounter = 0;
+    clearInterval(myFunc);
+    myFunc = setInterval(changeImage, 300);
+}
+
+function changeImage() {
+    photoCounter++;
+    photoString = `${photoCounter}.jpg`;
+    document.getElementById("photo").src=photoString;
+}
